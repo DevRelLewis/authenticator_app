6f6a09d9d961c220e7d7be278e8956aab7271693
Lewis Meyers <machinelewis@Lewiss-MacBook-Pro.local>
2024-09-23 11:43:44 -0400

    added new users and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_17_134739) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_22_201949) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email"
   end
 end
